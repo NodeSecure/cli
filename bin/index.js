386280629eb0772e24e43c03fdee3fbd99cf8d81
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-05 13:27:17 +0200

    refactor: add multiple CLI command (cwd & from)

--- bin/index.js
+++ bin/index.js
@@ -6,50 +6,50 @@ require("make-promises-safe");
 require("dotenv").config();
 
 // Require Node.js Dependencies
-const { readFileSync, accessSync, writeFileSync } = require("fs");
+const { writeFileSync } = require("fs");
 const { join } = require("path");
 const { performance } = require("perf_hooks");
 
 // Require Third-party Dependencies
 const sade = require("sade");
 const pacote = require("pacote");
-const { red, yellow, grey } = require("kleur");
+const { yellow, grey } = require("kleur");
 const ora = require("ora");
 
 // Require Internal Dependencies
 const { depWalker } = require("../src/depWalker");
+const nodeSecure = require("../index");
 
 // Process script arguments
-sade("nsecure [package]", true)
-    .version("0.1.0")
-    .describe("Run security analysis")
-    .option("-p, --port", "HTTP Server port", 1337)
-    .action(main)
-    .parse(process.argv);
+const prog = sade("nsecure").version("0.1.0");
+console.log(grey().bold(`\n > Executing node-secure at: ${yellow().bold(process.cwd())}\n`));
 
-async function main(packageName, opts) {
-    const CWD = process.cwd();
-    console.log(grey().bold(`\n > Executing node-secure at: ${yellow().bold(CWD)}`));
+function logAndWrite(payload) {
+    if (payload === null) {
+        console.log("No dependencies to proceed !");
 
-    let manifest = null;
-
-    // if there is no option "package", then
-    // read the package.json located at CWD + package.json
-    if (typeof packageName === "undefined") {
-        try {
-            const packagePath = join(CWD, "package.json");
-
-            accessSync(packagePath);
-            const str = readFileSync(packagePath, "utf-8");
-            manifest = JSON.parse(str);
-        }
-        catch (err) {
-            console.log(red().bold("Failed to read or parse local package.json !"));
-        }
+        return;
     }
 
-    // Else search the package in the npm registry with pacote
-    else {
+    const ret = JSON.stringify(Object.fromEntries(payload), null, 2);
+    console.log(`Number of dependencies: ${payload.size}`);
+    writeFileSync(join(__dirname, "..", "result.json"), ret);
+}
+
+prog
+    .command("cwd")
+    .describe("Run on the current working dir")
+    .action(async function cwd() {
+        const payload = await nodeSecure(void 0, true);
+        logAndWrite(payload);
+    });
+
+prog
+    .command("from <package>")
+    .describe("Run on the current working dir")
+    .action(async function from(packageName) {
+        let manifest = null;
+
         const spinner = ora(`Searching for '${yellow().bold(packageName)}' manifest in npm registry!`).start();
         try {
             const start = performance.now();
@@ -60,20 +60,13 @@ async function main(packageName, opts) {
         catch (err) {
             spinner.fail(err.message);
         }
-    }
 
-    if (manifest === null) {
-        return;
-    }
-    console.log("");
-    const dependencies = await depWalker(manifest);
-    if (dependencies === null) {
-        console.log("No dependencies to proceed !");
+        if (manifest === null) {
+            return;
+        }
 
-        return;
-    }
+        const payload = await depWalker(manifest);
+        logAndWrite(payload);
+    });
 
-    const ret = JSON.stringify(Object.fromEntries(dependencies), null, 2);
-    console.log(`Number of dependencies: ${dependencies.size}`);
-    writeFileSync(join(__dirname, "..", "result.json"), ret);
-}
+prog.parse(process.argv);
--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { join } = require("path");
 const { readFile, access } = require("fs").promises;
 
 // Require Internal Dependencies
-const depWalker = require("./src/depWalker");
+const { depWalker } = require("./src/depWalker");
 
-module.exports = async function nodesecure(cwd = process.cwd()) {
+module.exports = async function nodesecure(cwd = process.cwd(), verbose = false) {
     const packagePath = join(cwd, "package.json");
 
     await access(packagePath);
     const str = await readFile(packagePath, "utf-8");
     const manifest = JSON.parse(str);
 
-    return depWalker(manifest, { verbose: false });
+    return depWalker(manifest, { verbose });
 };
