e6a6044e87de07787c783ef63dfc304778cc033f
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-05 12:53:38 +0200

    test: depWalk on slimio/is

---
+++ test/depWalker.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Require Node.js Dependencies
+const { join } = require("path");
+
+// Require Internal Dependencies
+const { depWalker } = require("../src/depWalker");
+
+// CONSTANTS
+const FIXTURE_PATH = join(__dirname, "fixtures/depWalker");
+
+// JSON PAYLOADS
+const is = require(join(FIXTURE_PATH, "slimio.is.json"));
+
+test("should return null for depWalker of @slimio/is", async() => {
+    const result = await depWalker(is, { verbose: false });
+    expect(result).toStrictEqual(null);
+});
