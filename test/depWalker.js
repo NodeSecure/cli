373cb29eb6c26285912d0f893b549a117dcaceff
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-23 10:47:37 +0200

    test: fix depWalker

--- test/depWalker.js
+++ test/depWalker.js
@@ -11,8 +11,10 @@ const FIXTURE_PATH = join(__dirname, "fixtures/depWalker");
 
 // JSON PAYLOADS
 const is = require(join(FIXTURE_PATH, "slimio.is.json"));
+const payload = require(join(FIXTURE_PATH, "slimio.is-result.json"));
 
 test("should return null for depWalker of @slimio/is", async() => {
     const result = await depWalker(is, { verbose: false });
-    expect(result).toStrictEqual(null);
+    const resultAsJSON = JSON.parse(JSON.stringify(Object.fromEntries(result), null, 2));
+    expect(resultAsJSON).toMatchObject(payload);
 });
