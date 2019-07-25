7b58943c616a68a4a620821a999abe16283c5261
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-25 17:31:13 +0200

    test: use snapshot for depWalker & remove .size property

--- test/depWalker.js
+++ test/depWalker.js
@@ -11,10 +11,10 @@ const FIXTURE_PATH = join(__dirname, "fixtures/depWalker");
 
 // JSON PAYLOADS
 const is = require(join(FIXTURE_PATH, "slimio.is.json"));
-const payload = require(join(FIXTURE_PATH, "slimio.is-result.json"));
 
 test("should return null for depWalker of @slimio/is", async() => {
     const result = await depWalker(is, { verbose: false });
     const resultAsJSON = JSON.parse(JSON.stringify(Object.fromEntries(result), null, 2));
-    expect(resultAsJSON).toMatchObject(payload);
+    delete resultAsJSON["@slimio/is"]["1.5.1"].size;
+    expect(resultAsJSON).toMatchSnapshot();
 });
