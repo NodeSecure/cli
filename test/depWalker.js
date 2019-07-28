5592421977d9843b59715e7ea0ab9d7127c53408
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-28 13:14:19 +0200

    test: sort extensions

--- test/depWalker.js
+++ test/depWalker.js
@@ -17,6 +17,7 @@ test("should return null for depWalker of @slimio/is", async() => {
     const resultAsJSON = JSON.parse(JSON.stringify(Object.fromEntries(result), null, 2));
 
     // TODO: how can we test the complete payload without breaking depending the OS ?
+    resultAsJSON["@slimio/is"]["1.5.1"].composition.extensions.sort();
     delete resultAsJSON["@slimio/is"]["1.5.1"].size;
     delete resultAsJSON["@slimio/is"]["1.5.1"].composition.files;
 
