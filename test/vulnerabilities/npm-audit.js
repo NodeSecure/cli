8a6eb33c816471455940d805e350376842789c42
Antoine Coulon <coulon_antoine@outlook.fr>
2021-03-06 21:23:32 +0100

    fix: remove unused callback.

--- test/vulnerabilities/npm-audit.js
+++ test/vulnerabilities/npm-audit.js
@@ -3,7 +3,7 @@
 // Require Internal Dependencies
 const NPMAuditStrategy = require("../../src/vulnerabilities/strategies/npm-audit");
 
-test("Run NPM Audit and get vulnerabilities", async(done) => {
+test("Run NPM Audit and get vulnerabilities", async() => {
     const vulnStrategy = NPMAuditStrategy();
     await vulnStrategy.hydrateNodeSecurePayload({});
 });
