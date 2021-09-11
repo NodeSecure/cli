bf7130500e8c7fb112ec889ec48d12d00c2b7b85
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2021-09-11 18:23:51 +0200

    refactor(lang): use new i18n.getLanguages method

--- src/commands/lang.js
+++ src/commands/lang.js
@@ -3,22 +3,16 @@ import * as i18n from "@nodesecure/i18n";
 import qoa from "qoa";
 import kleur from "kleur";
 
-const { white, green, yellow } = kleur;
-
 export async function set() {
-  const currentLang = i18n.getLocalLang();
-  const langs = ["french", "english"];
-
-  langs.splice(langs.indexOf(currentLang), 1);
-  langs.unshift(currentLang);
-
   console.log("");
   const { selectedLang } = await qoa.interactive({
-    query: green().bold(` ${i18n.getToken("cli.commands.lang.question_text")}`),
+    query: kleur.green().bold(` ${i18n.getToken("cli.commands.lang.question_text")}`),
     handle: "selectedLang",
-    menu: langs
+    menu: i18n.getLanguages()
   });
 
   await i18n.setLocalLang(selectedLang);
-  console.log(white().bold(`\n ${i18n.getToken("cli.commands.lang.new_selection", yellow().bold(selectedLang))}`));
+  console.log(
+    kleur.white().bold(`\n ${i18n.getToken("cli.commands.lang.new_selection", kleur.yellow().bold(selectedLang))}`)
+  );
 }
