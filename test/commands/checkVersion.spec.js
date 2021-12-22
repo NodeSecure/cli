05e1e0e0db7ba9ec9a4a02c4cb0e74f05f28ca30
tony-go <gorez.tony@gmail.com>
2021-12-22 21:24:20 +0100

    fix: uncomment tests

--- test/commands/checkVersion.spec.js
+++ test/commands/checkVersion.spec.js
@@ -4,25 +4,23 @@ import test from "tape";
 // Import Internal dependencies
 import { buildVersionChecker } from "../../src/commands/checkVersion.js";
 
-// test("isAnalysisVersionValid should return true when the version satisfies the range", (tape) => {
-//   const isPackageValid = buildVersionChecker(">=1.0.0");
+test("isAnalysisVersionValid should return true when the version satisfies the range", (tape) => {
+  const isPackageValid = buildVersionChecker(">=1.0.0");
 
-//   console.log("o", isPackageValid("1.0.0"));
-//   tape.true(true);
-//   // tape.true(isPackageValid("1.0.1"));
-//   // tape.true(isPackageValid("2.0.1-0"));
-//   // tape.true(isPackageValid("3.6.7.9.3-alpha"));
+  tape.true(isPackageValid("1.0.1"));
+  tape.true(isPackageValid("2.0.1-0"));
+  tape.true(isPackageValid("3.6.7.9.3-alpha"));
 
-//   tape.end();
-// });
+  tape.end();
+});
 
-// test("isAnalysisVersionValid should return false when the version doesn't satisfies the range", (tape) => {
-//   const isPackageValid = buildVersionChecker("<=1.0.0");
+test("isAnalysisVersionValid should return false when the version doesn't satisfies the range", (tape) => {
+  const isPackageValid = buildVersionChecker("<=1.0.0");
 
-//   tape.false(isPackageValid("1.0.1"));
-//   tape.false(isPackageValid("2.0.1"));
-//   tape.false(isPackageValid("3.0.1-0"));
-//   tape.false(isPackageValid("3.6.7.9.3-alpha"));
+  tape.false(isPackageValid("1.0.1"));
+  tape.false(isPackageValid("2.0.1"));
+  tape.false(isPackageValid("3.0.1-0"));
+  tape.false(isPackageValid("3.6.7.9.3-alpha"));
 
-//   tape.end();
-// });
+  tape.end();
+});
