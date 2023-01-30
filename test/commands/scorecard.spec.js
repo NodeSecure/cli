c9466db63a9c22d38d5be0ab5e22ba34bebbfdc3
PierreDemailly <pierredemailly.pro@gmail.com>
2023-01-30 23:47:06 +0100

    fix: remove duplicate test

--- test/commands/scorecard.spec.js
+++ test/commands/scorecard.spec.js
@@ -75,37 +75,3 @@ test("should retrieve repository whithin git config", async(tape) => {
   tape.equal(getCurrentRepository(), "NodeSecure/cli");
   tape.end();
 });
-
-test("scorecard should display fastify scorecard", async(tape) => {
-  const pkgName = "fastify/fastify";
-  const mockBody = {
-    date: "2222-12-31",
-    repo: {
-      name: `github.com/${pkgName}`
-    },
-    score: 5.2,
-    checks: [
-      {
-        name: "Maintained",
-        score: -1,
-        reason: "Package is maintained"
-      }
-    ]
-  };
-  const scorecardCliOptions = {
-    path: kProcessPath,
-    packageName: pkgName,
-    api: {
-      baseUrl: kOpenSSFScorecardRestApi,
-      shouldFail: false,
-      response: { body: mockBody }
-    }
-  };
-
-  const { forkAndGetLines, mockApiOptions } = initCliRunner(scorecardCliOptions);
-  const expectedLines = getExpectedScorecardLines(pkgName, mockApiOptions.response.body);
-  const givenLines = await forkAndGetLines();
-
-  tape.deepEqual(givenLines, expectedLines, `lines should be ${expectedLines}`);
-  tape.end();
-});
