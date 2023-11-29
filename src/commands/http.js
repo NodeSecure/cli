f15dc344f9a3c78ffc9963173e64e06703c188fa
fraxken <gentilhomme.thomas@gmail.com>
2023-11-29 17:52:55 +0100

    feat: verify payload scanner version & implement local i18n

--- src/commands/http.js
+++ src/commands/http.js
@@ -1,8 +1,17 @@
 // Import Node.js Dependencies
+import fs from "node:fs";
 import path from "node:path";
+
+// Import Third-party Dependencies
+import * as SemVer from "semver";
+import kleur from "kleur";
+import * as i18n from "@nodesecure/i18n";
 
 // Import Internal Dependencies
 import { buildServer } from "../http-server/index.js";
+
+// CONSTANTS
+const kRequiredScannerRange = ">=5.1.0";
 
 export async function start(
   payloadFileBasename = "nsecure-result.json",
@@ -18,6 +27,7 @@ export async function start(
     process.cwd(),
     fileExtension === "" ? `${payloadFileBasename}.json` : payloadFileBasename
   );
+  assertScannerVersion(dataFilePath);
 
   const httpServer = buildServer(dataFilePath, {
     port: Number.isNaN(port) ? 0 : port
@@ -25,5 +35,28 @@ export async function start(
 
   for (const eventName of ["SIGINT", "SIGTERM"]) {
     process.on(eventName, () => httpServer.server.close());
+  }
+}
+
+function assertScannerVersion(
+  dataFilePath
+) {
+  const rawStr = fs.readFileSync(dataFilePath, "utf-8");
+  const { scannerVersion } = JSON.parse(rawStr);
+
+  if (!SemVer.satisfies(scannerVersion, kRequiredScannerRange)) {
+    const error = i18n.getTokenSync(
+      "localI18n.startHttp.invalidScannerVersion",
+      kleur.yellow(scannerVersion),
+      kleur.yellow(kRequiredScannerRange)
+    );
+    const regenerate = i18n.getTokenSync("localI18n.startHttp.regenerate");
+
+    console.log(" > " + path.basename(dataFilePath));
+    console.log(" > " + kleur.red().bold(error));
+    console.log(` > ${regenerate}`);
+    console.log();
+
+    process.exit(0);
   }
 }
