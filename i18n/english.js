e9873e214a80262ef854486ee7bcc818475e7f04
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2025-06-15 12:48:53 +0200

    fix: css on 'maintainers popup separator' and 'package cache result' (#515)

--- i18n/english.js
+++ i18n/english.js
@@ -81,7 +81,7 @@ const cli = {
   startHttp: {
     invalidScannerVersion: tS`the payload has been scanned with version '${0}' and do not satisfies the required CLI range '${1}'`,
     regenerate: "please re-generate a new JSON payload using the CLI"
-  },
+  }
 };
 
 const ui = {
