069bbb715bb6e9a0142e692d25b4ccf2670ea073
cgombauld <clement.gombauld@cpexterne.org>
2025-08-05 19:30:33 +0200

    feat(icon): implement new icon system

--- public/components/expandable/expandable.js
+++ public/components/expandable/expandable.js
@@ -1,11 +1,40 @@
 // Import Third-party Dependencies
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { when } from "lit/directives/when.js";
 
 // Import Internal Dependencies
 import { currentLang } from "../../common/utils";
+import "../icon/icon.js";
 
 class Expandable extends LitElement {
+  static styles = css`
+span.expandable {
+  display: flex;
+  align-items: center !important;
+  justify-content: center !important;
+  height: 35px;
+  font-size: 13px;
+  font-family: mononoki;
+  background: none;
+  color: #00B0FF;
+  text-shadow: 1px 1px 1px rgb(20 20 20 / 50%);
+  transition: all 0.2s linear;
+  margin-top: 5px;
+}
+
+span.expandable[data-value="opened"] {
+  color: #F44336 !important;
+}
+
+span.expandable:hover {
+  cursor: pointer;
+}
+
+span.expandable nsecure-icon {
+  margin-right: 4px;
+  margin-top: 1px;
+}
+`;
   static properties = {
     onToggle: { type: Function },
     isClosed: { type: Boolean }
@@ -17,20 +46,15 @@ class Expandable extends LitElement {
     this.onToggle = () => void 0;
   }
 
-  // FIXME: must opt out from the shadow DOM for now because of to be able to apply CSS from fontello
-  createRenderRoot() {
-    return this;
-  }
-
   render() {
     const lang = currentLang();
 
     return html`
       <span data-value=${this.isClosed ? "opened" : "closed"}  @click=${this.#handleClick} class="expandable">
         ${when(this.isClosed,
-          () => html`<i class="icon-minus-squared-alt"></i>
+          () => html`<nsecure-icon name="minus"></nsecure-icon>
         <p>${window.i18n[lang].home.showLess}</p>`,
-          () => html`<i class="icon-plus-squared-alt"></i>
+          () => html`<nsecure-icon name="plus"></nsecure-icon>
         <p>${window.i18n[lang].home.showMore}</p>`
         )}
       </span>
