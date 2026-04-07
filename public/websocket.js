be0f4f7d07546e9478cabe608745adbca587cae3
ZeEarth <zeearth@gmail.com>
2026-04-07 09:26:56 +0200

    enhance type safety for websocket.js

--- public/websocket.js
+++ public/websocket.js
@@ -1,17 +1,23 @@
+/**
+ * @typedef {string | object} Spec
+ */
 
 export class WebSocketClient extends EventTarget {
   /** @type {WebSocket} */
   client;
 
   constructor(
+    /** @type {String} */
     url
   ) {
     super();
     this.client = new WebSocket(url);
     this.client.addEventListener("message", this.#messageHandler.bind(this));
     this.commands = {
-      search: (spec) => this.send({ commandName: "SEARCH", spec }),
-      remove: (spec) => this.send({ commandName: "REMOVE", spec })
+      search: /** @param {Spec} spec */ (spec) =>
+        this.send({ commandName: "SEARCH", spec }),
+      remove: /** @param {Spec} spec */ (spec) =>
+        this.send({ commandName: "REMOVE", spec }),
     };
 
     window.socket = this;
@@ -20,25 +26,25 @@ export class WebSocketClient extends EventTarget {
     };
   }
 
+  /** @type {(data: Record<string, any>) => void} */
   send(data) {
     this.client.send(JSON.stringify(data));
   }
 
+  /**
+   * @param {MessageEvent} event
+   */
   #messageHandler(event) {
+    /** @type {{ status: string, [key: string]: any }} */
     const data = JSON.parse(event.data);
     if (!data.status) {
-      console.warn(
-        "[WEBSOCKET] Received data without status:",
-        data
-      );
+      console.warn("[WEBSOCKET] Received data without status:", data);
 
       return;
     }
 
     console.log(`[WEBSOCKET] data status = '${data.status}'`);
-    this.dispatchEvent(
-      new CustomEvent(data.status, { detail: data })
-    );
+    this.dispatchEvent(new CustomEvent(data.status, { detail: data }));
   }
 
   close() {
