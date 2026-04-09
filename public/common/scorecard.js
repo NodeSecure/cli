71798e153758946d83d02a463cfdf4f1b5d9d9a3
ZeEarth <zeearth@gmail.com>
2026-04-09 23:54:44 +0200

    add missing type for data. Create a ScorecardData typeDef event only score.data is used at the moment.

--- public/common/scorecard.js
+++ public/common/scorecard.js
@@ -2,11 +2,55 @@
 import { getJSON } from "@nodesecure/vis-network";
 
 /**
+ * @typedef {Object} ScorecardCheckDocumentation
+ * @property {string} short
+ * @property {string} url
+ */
+
+/**
+ * @typedef {Object} ScorecardCheck
+ * @property {string} name
+ * @property {number} score
+ * @property {string} reason
+ * @property {string[] | null} details
+ * @property {ScorecardCheckDocumentation} documentation
+ */
+
+/**
+ * @typedef {Object} ScorecardRepo
+ * @property {string} name
+ * @property {string} commit
+ */
+
+/**
+ * @typedef {Object} ScorecardTool
+ * @property {string} version
+ * @property {string} commit
+ */
+
+/**
+ * @typedef {Object} ScorecardData
+ * @property {string} date
+ * @property {ScorecardRepo} repo
+ * @property {ScorecardTool} scorecard
+ * @property {number} score
+ * @property {ScorecardCheck[]} checks
+ */
+
+/**
  * @param {!string} repoName
+ * @param {string} platform
+ * @returns {Promise<ScorecardData | null>}
  */
 export async function fetchScorecardData(repoName, platform = "github.com") {
   try {
-    const { data } = (await getJSON(`/scorecard/${repoName}?platform=${platform}`));
+    /** @type any responseRaw */
+    const responseRaw = (await getJSON(`/scorecard/${repoName}?platform=${platform}`));
+
+    /** @type {{ data: ScorecardData }} */
+    const response = responseRaw;
+
+    const data = response.data;
     if (!data) {
       return null;
     }
